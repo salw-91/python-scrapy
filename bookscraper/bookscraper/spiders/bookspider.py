c2b3875f40fa436ee9f10e7597928617602614b7
Salwan-Aldulaimi <salwan@holygrow.nl>
2023-06-11 12:27:34 +0200

    Add book info

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -20,6 +20,20 @@ class BookspiderSpider(scrapy.Spider):
             yield response.follow(book_url, callback = self.parse_book_page)
 
     def parse_book_page(self, response):
+
+        table_rows = response.css('table tr')
+
+        yield {
+            'url': response.url,
+            'title' : response.css('.product_main h1::text').get(),
+            'product_type' : table_rows[1].css('td ::text').get(),
+            'price_excl_tax' : table_rows[2].css('td ::text').get(),
+            'price_incl_tax' : table_rows[3].css('td ::text').get(),
+            'tax' : table_rows[4].css('td ::text').get(),
+            'availability' : table_rows[5].css('td ::text').get(),
+            'num_reviews' : table_rows[6].css('td ::text').get(),
+        }
+
         pass
 
 
