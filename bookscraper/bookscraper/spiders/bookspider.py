1ba3ac87490d3290ab0d737a2d395061ffbb7bde
Salwan-Aldulaimi <salwan@holygrow.nl>
2023-06-11 11:23:19 +0200

    first commit

---
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,28 @@
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response):
+        books = response.css('article.product_pod')
+
+        for book in books:
+            relative_url = book.css('h3 a ::attr(href)').get()
+
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+
+            yield response.follow(book_url, callback = self.parse_book_page)
+
+    def parse_book_page(self, response):
+        pass
+
+
+
+        
+
